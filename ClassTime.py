77caf7222cb13c48faceea4e80494589e4ee1ed4
jerry-hannn <109703519+jerry-hannn@users.noreply.github.com>
2022-08-14 11:31:16 -0500

    Update ClassTime.py

--- ClassTime.py
+++ ClassTime.py
@@ -1,19 +1,26 @@
 # My first attempt at calling APIs!
+# bunch of imports
 import requests
 from datetime import datetime
 import time
 import googlemaps
 import os
-from dotenv import load_dotenv
 import config
-load_dotenv()
 
-api_key = config.GMAPS_API_KEY
-print(type(api_key))
+
+api_key = os.environ.get('GMaps_API_KEY')
+
 url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=South%Mid%Quads&destinations=2145%Technological%Institute&units=imperial&mode=walking&key=' + api_key
 
-payload = {}
-headers = {}
-response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url)
 
-print(response.text)
+split = response.text.splitlines()
+for line in split:
+    stripped = line.strip()
+    if stripped.startswith('"origin_addresses"'):
+        print("From " + stripped[24:len(stripped) - 4])
+    if stripped.startswith('"destination_addresses"'):
+        print("To " + stripped[29:len(stripped) - 4])
+    if stripped.startswith('"text"'):
+        print (stripped[10:len(stripped) - 2])
+#print(response.text)
