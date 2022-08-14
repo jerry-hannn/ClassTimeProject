fe8825903bffc1c066714f985b8a94a3f0d81d3d
jerry-hannn <109703519+jerry-hannn@users.noreply.github.com>
2022-08-13 23:11:05 -0500

    downloading a bunch of stuff

--- ClassTime.py
+++ ClassTime.py
@@ -1,2 +1,9 @@
 # My first attempt at calling APIs!
 import requests
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=AIzaSyBImgfFw09uTSDp_KhP7egw5zAym8Ah3ks"
+
+payload = {}
+headers = {}
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
