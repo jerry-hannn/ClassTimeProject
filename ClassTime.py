b8fc85e13a1b272fc02c7cc484f308dda7e9fa9b
jerry-hannn <109703519+jerry-hannn@users.noreply.github.com>
2022-08-14 00:03:13 -0500

    a lot

--- ClassTime.py
+++ ClassTime.py
@@ -1,6 +1,10 @@
 # My first attempt at calling APIs!
 import requests
-url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=AIzaSyBImgfFw09uTSDp_KhP7egw5zAym8Ah3ks"
+from datetime import datetime
+import time
+import googlemaps
+
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=South%Mid%Quads&destinations=2145%Technological%Institute&units=imperial&mode=walking&key=AIzaSyBImgfFw09uTSDp_KhP7egw5zAym8Ah3ks"
 
 payload = {}
 headers = {}
