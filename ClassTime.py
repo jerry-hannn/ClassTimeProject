f72cfeb3255ad00de932930eb457beec3935769a
jerry-hannn <109703519+jerry-hannn@users.noreply.github.com>
2022-08-15 21:00:45 -0500

    hardcoded :(

--- ClassTime.py
+++ ClassTime.py
@@ -1,4 +1,6 @@
-# My first attempt at calling APIs!
+# My first attempt at calling APIs! Currently, this can take input of the few hardcoded classes in my schedule and a couple of dorms and other buildings.
+# This only supports point to point currently.
+# This will eventually export data in some form, currently, it only exports the addresses and then the distance and walking time.
 # bunch of imports
 import requests
 from datetime import datetime
@@ -8,18 +10,38 @@ import os
 import LocationData
 
 def distances():
-    origin = input("Enter class/dorm you are departing from: ")
-    destination = input("Enter class/dorm you are going to: ")
+    origin = input("Enter class/dorm/building you are departing from: ").lower()
+    destination = input("Enter class/dorm/building you are going to: ").lower()
     if origin in LocationData.Dorms:
         origin_address = LocationData.Dorms.get(origin)
     elif origin in LocationData.cs:
         origin_address = LocationData.cs.get(origin)
     elif origin in LocationData.dtc:
         origin_address = LocationData.dtc.get(origin)
+    elif origin in LocationData.math:
+        origin_address = LocationData.math.get(origin)
+    elif origin in LocationData.ea:
+        origin_address = LocationData.ea.get(origin)
+    elif origin in LocationData.other:
+        origin_address = LocationData.other.get(origin)
+
+
+    if destination in LocationData.Dorms:
+        destination_address = LocationData.Dorms.get(destination)
+    elif destination in LocationData.cs:
+        destination_address = LocationData.cs.get(destination)
+    elif destination in LocationData.dtc:
+        destination_address = LocationData.dtc.get(destination)
+    elif destination in LocationData.math:
+        destination_address = LocationData.math.get(destination)
+    elif destination in LocationData.ea:
+        destination_address = LocationData.ea.get(destination)
+    elif destination in LocationData.other:
+        destination_address = LocationData.other.get(destination)
     # Continue doing this elif stuff, find a way to do it better later
 
     api_key = os.environ.get('GMaps_API_KEY')
-    url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=South%Mid%Quads&destinations=2145%Technological%Institute&units=imperial&mode=walking&key=' + api_key
+    url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origin_address + '&destinations=2145%' + destination_address + '&units=imperial&mode=walking&key=' + api_key
 
     response = requests.request("GET", url)
 
@@ -33,3 +55,4 @@ def distances():
         elif stripped.startswith('"text"'):
             print (stripped[10:len(stripped) - 2])
 #print(response.text)
+distances()
--- LocationData.py
+++ LocationData.py
@@ -3,16 +3,16 @@ ea = {
     "ea1_dis":"Frances%Searle$Building"
 }
 cs = {
-    "111-0":"Technological%Institute",
-    "211-0":"Technological%Institute"
+    "cs 111-0":"Technological%Institute",
+    "cs 211-0":"Technological%Institute"
 }
 math = {
-    "220-1":"Technological%Institute",
-    "220-1_dis":"Lunt%Hall"
+    "math 220-1":"Technological%Institute",
+    "math 220-1_dis":"Lunt%Hall"
 }
 dtc = {
-    "lecture":"Ford%Motor$Company$Engineering$Design$Center",
-    "dis":"Technological%Institute"
+    "dtc lecture":"Ford%Motor$Company$Engineering$Design$Center",
+    "dtc dis":"Technological%Institute"
 }
 Dorms = {
     "636 emerson":"636%Emerson%St%Evanston%Illinois",
@@ -24,4 +24,13 @@ Dorms = {
     "1856 orrington":"1856%Orrington%Ave%Evanston%Illinois",
     "1856 orrington ave":"1856%Orrington%Ave%Evanston%Illinois",
     "SMQ":"South%Mid%Quads"
+}
+other = {
+    "spac":"Henry%Crown%Sports%Pavilion",
+    "mudd":"Mudd%Library",
+    "blom":"Blomquist%Recreation%Center",
+    "main":"1970%Campus%Dr$Evanston$Illinois",
+    "norris":"Norris%University%Center",
+    "kellogg":"Kellogg%Global%Hub",
+    "bienen":"Bienen%School%Of%Music",
 }
